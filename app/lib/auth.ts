bfaf36ab6a42fbd8e42f38dceef0128c3284e284
Utkarsh Prakash <114054236+Utkarsh434@users.noreply.github.com>
2025-08-24 22:31:49 +0530

    4th

---
+++ app/lib/auth.ts
@@ -0,0 +1,83 @@
+// app/lib/auth.ts
+import { AuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import prisma from '@/app/lib/prisma';
+import bcrypt from 'bcrypt';
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Invalid credentials');
+        }
+
+        // Warden Login Logic
+        if (credentials.email === process.env.WARDEN_EMAIL) {
+          if (credentials.password === process.env.WARDEN_PASSWORD) {
+            let wardenUser = await prisma.user.findUnique({
+              where: { email: process.env.WARDEN_EMAIL },
+            });
+            if (!wardenUser) {
+              wardenUser = await prisma.user.create({
+                data: {
+                  name: 'Hostel Warden',
+                  email: process.env.WARDEN_EMAIL!,
+                  password: await bcrypt.hash(process.env.WARDEN_PASSWORD!, 10),
+                  role: 'WARDEN',
+                },
+              });
+            }
+            return wardenUser;
+          } else {
+            throw new Error('Invalid credentials');
+          }
+        }
+
+        // Regular User Login Logic
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+        if (!user || !user?.password) {
+          throw new Error('Invalid credentials');
+        }
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+        if (!isCorrectPassword) {
+          throw new Error('Invalid credentials');
+        }
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.role = token.role as string;
+      }
+      return session;
+    },
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
